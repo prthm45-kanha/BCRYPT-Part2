0353e2b326bc3f98b5a2d4bbedecdb0d5a447712
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-16 20:00:46 +0530

    applied hash to password

--- app.js
+++ app.js
@@ -17,6 +17,21 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     res.render('index');
 })
+
+app.post('/create',(req,res)=>{
+    let {username,email,password,age}=req.body;
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,async (err,hash)=>{
+            let createdUser=await userModel.create({
+                username,
+                email,
+                password:hash,
+                age
+            });
+            res.send(createdUser)
+        })
+    })
+})
 app.listen(3000,()=>{
     console.log("Running on port 3000");
 })
